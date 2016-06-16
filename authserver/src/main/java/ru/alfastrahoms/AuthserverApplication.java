0b0b58d4ca44d23fd49f6045b5e8f07debdb199e
ScRsa <ScRsa@Mail.ru>
2016-06-16 22:51:54 +0700

    logback + logstash

--- authserver/src/main/java/ru/alfastrahoms/AuthserverApplication.java
+++ authserver/src/main/java/ru/alfastrahoms/AuthserverApplication.java
@@ -1,5 +1,7 @@
 package ru.alfastrahoms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.annotation.PostConstruct;
 import java.security.Principal;
 
 @SpringBootApplication
@@ -18,9 +21,20 @@ import java.security.Principal;
 @EnableResourceServer
 public class AuthserverApplication extends WebMvcConfigurerAdapter {
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthserverApplication.class);
+
+    @PostConstruct
+    public void logSomething() {
+        logger.debug("DebugDebugDebugDebugDebugDebugDebugDebugDebugDebugDebugDebugDebugDebug");
+        logger.info("InfoInfoInfoInfoInfoInfoInfoInfoInfoInfoInfoInfoInfoInfoInfoInfoInfoInfo");
+        logger.error("ErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorError");
+        logger.trace("TraceTraceTraceTraceTraceTraceTraceTraceTraceTraceTraceTraceTraceTrace");
+    }
+
     @RequestMapping("/user")
     @ResponseBody
     public Principal user(Principal user) {
+        logger.info("test logger system. user {}", user);
         return user;
     }
 
